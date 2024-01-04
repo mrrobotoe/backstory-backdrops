979a3f886c1760e458030a44ac4165bfe63f138e
mrrobotoe <aldo.martinez@my.utsa.edu>
2024-01-04 14:59:37 -0600

    added changes for deployment

--- src/ui/routes/RootLayout/RootLayout.tsx
+++ src/ui/routes/RootLayout/RootLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import { Navigation } from '../../components/Navigation';
-import React from 'react';
 import { Footer } from '../../components/Footer';
 
 export const RootLayout = () => {
